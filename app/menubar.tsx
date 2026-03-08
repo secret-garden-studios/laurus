b811ac58522d280d164004b0d42a10291bce6474
stef totino <stefantotino@gmail.com>
2026-03-08 11:53:04 -0400

    use full name text in menubar instead of a logo

--- app/menubar.tsx
+++ app/menubar.tsx
@@ -1,23 +1,23 @@
 'use client'
-import Image from "next/image";
+import { dellaRespira } from "./fonts";
 
 export default function Menubar() {
     return (
         <>
-            <div style={{
-                height: 50,
-                display: "flex",
-                alignItems: "center",
-                background: "linear-gradient(360deg, rgba(8, 8, 8, 1), rgba(27, 27, 27, 1))",
-                boxShadow: "rgba(255, 255, 255, 0.05) 0px 0px 30px 1px",
-                paddingLeft: 12
-            }}>
-                <Image
-                    style={{ borderRadius: 5, border: '1px solid rgba(255, 255, 255, 0.08)' }}
-                    src={"/laurus-logo-dark-theme.png"}
-                    alt={"logo"}
-                    width={32}
-                    height={32} />
+            <div
+                className={dellaRespira.className}
+                style={{
+                    height: 50,
+                    display: "flex",
+                    alignItems: "center",
+                    background: "linear-gradient(360deg, rgba(8, 8, 8, 1), rgba(27, 27, 27, 1))",
+                    boxShadow: "rgba(255, 255, 255, 0.05) 0px 0px 30px 1px",
+                    paddingLeft: 12,
+                    fontSize: 20,
+                    letterSpacing: '1px',
+                    color: 'rgb(227,227,227)'
+                }}>
+                {'Laurus'}
             </div>
         </>
     );
